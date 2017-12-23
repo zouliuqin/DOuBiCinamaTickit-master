32f037645adbbed7e8b3ed8d817dae84d0e29956
zouliuqin <511068289@qq.com>
2017-12-23 13:25:59 +0800

    2017.12.23  13：25

---
+++ app/src/main/java/com/liucheng/administrator/doubicinamatickit/module/BasicSettings.java
@@ -0,0 +1,15 @@
+package com.liucheng.administrator.doubicinamatickit.module;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.liucheng.administrator.doubicinamatickit.R;
+
+public class BasicSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_settings);
+    }
+}
--- app/src/main/java/com/liucheng/administrator/doubicinamatickit/module/myself/MyFragment.java
+++ app/src/main/java/com/liucheng/administrator/doubicinamatickit/module/myself/MyFragment.java
@@ -7,10 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.liucheng.administrator.doubicinamatickit.R;
 import com.liucheng.administrator.doubicinamatickit.fragment.BaseFragment;
+import com.liucheng.administrator.doubicinamatickit.module.BasicSettings;
 import com.liucheng.administrator.doubicinamatickit.module.login.LoginActivity;
 
 import butterknife.BindView;
@@ -34,6 +37,20 @@ public class MyFragment extends BaseFragment {
     Unbinder unbinder;
     @BindView(R.id.imageView_actionbar)
     ImageView imageViewActionbar;
+    @BindView(R.id.tv_my_islogin_or_username)
+    TextView tvMyIsloginOrUsername;
+    @BindView(R.id.ll_wanna_see)
+    LinearLayout llWannaSee;
+    @BindView(R.id.ll_have_seen)
+    LinearLayout llHaveSeen;
+    @BindView(R.id.ll_invite_friends)
+    LinearLayout llInviteFriends;
+    @BindView(R.id.ll_setting)
+    LinearLayout llSetting;
+    @BindView(R.id.ll_feedback)
+    LinearLayout llFeedback;
+    @BindView(R.id.ll_about_us)
+    LinearLayout llAboutUs;
 
     @Nullable
     @Override
@@ -56,8 +73,6 @@ public class MyFragment extends BaseFragment {
     }
 
 
-
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -65,19 +80,19 @@ public class MyFragment extends BaseFragment {
     }
 
 
-    @OnClick({R.id.imageView_actionbar, R.id.iv_my_fragment_my_head_portrait})
+    @OnClick({R.id.imageView_actionbar, R.id.iv_my_fragment_my_head_portrait,R.id.ll_wanna_see, R.id.ll_have_seen, R.id.ll_invite_friends, R.id.ll_setting, R.id.ll_feedback, R.id.ll_about_us})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.imageView_actionbar:
                 //判断用户是否登录。如果没有登录 跳转到登录界面，如果登录了 跳转到编辑个人资料页面
 
                 BmobUser bmobUser = BmobUser.getCurrentUser();
-                if(bmobUser != null){
-                    Intent intent2 = new Intent(getActivity(),MySetActivity.class);
+                if (bmobUser != null) {
+                    Intent intent2 = new Intent(getActivity(), MySetActivity.class);
                     startActivity(intent2);
-                }else{
+                } else {
                     Toast.makeText(getActivity(), "请先登录！", Toast.LENGTH_SHORT).show();
-                    Intent intent2 = new Intent(getActivity(),LoginActivity.class);
+                    Intent intent2 = new Intent(getActivity(), LoginActivity.class);
                     startActivity(intent2);
                 }
 
@@ -87,6 +102,32 @@ public class MyFragment extends BaseFragment {
                 Intent intent = new Intent(getContext(), LoginActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.ll_wanna_see:
+                //TODO  想看的电影
+                break;
+            case R.id.ll_have_seen:
+                //TODO  看过的电影
+                break;
+            case R.id.ll_invite_friends:
+                //TODO  分享给好友
+                break;
+            case R.id.ll_setting:
+                //TODO  基本设置
+                Intent  intent1 = new Intent(getActivity(), BasicSettings.class);
+                startActivity(intent1);
+                break;
+            case R.id.ll_feedback:
+                //TODO  反馈
+                break;
+            case R.id.ll_about_us:
+                //TODO  关于我们
+                break;
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //TODO  退出登录后要重新检查用户是否登录，如果没有登录 则修改界面为未登录样式
     }
 }
